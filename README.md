f206f4991531228d908997b45cfafadecfa34568
Curtis Laurence Chadwell <43461325+curchadw@users.noreply.github.com>
2021-05-17 19:27:30 +0000

    Added a note to the readme adn will update overtime

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Social_media_clone
+
+To clarify what is going on with this, I am trying to learn how to use React Native to gain some experience in mobile development. At the moment I don't have a lo to comment on this yet, but this should turn into something at some point.
--- components/Home.js
+++ components/Home.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, {useState}  from 'react';
+import { Text, View, Button, ScrollView } from 'react-native';
 
 const HelloWorldApp = () => {
+    const [count, setCount] = useState(0)
   return (
+  <ScrollView>
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center"
       }}>
-      <Text>Hello, world!</Text>
+      <Text>Count: {count}</Text>
+      <Button onPress={()=> setCount(count + 1)} title='Click ME'/>
+
+
     </View>
+  </ScrollView>
   )
 }
 export default HelloWorldApp;
